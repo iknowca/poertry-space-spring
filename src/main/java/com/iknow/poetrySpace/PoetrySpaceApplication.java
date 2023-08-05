5ebc0abbe7d8cfae3740523807f645055de9256d
iknowca <iknowca@gmail.com>
2023-08-05 14:06:53 +0900

    init: [김원기] spring 환경설정 [PSS-12]

---
+++ src/main/java/com/iknow/poetrySpace/PoetrySpaceApplication.java
@@ -0,0 +1,13 @@
+package com.iknow.poetrySpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoetrySpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoetrySpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iknow/poetrySpace/PoetrySpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iknow.poetrySpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoetrySpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
